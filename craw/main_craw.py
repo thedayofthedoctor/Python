89ef444b69582a2d4f4d1b3a142215264f126fa2
Matt Belfast Brown <thedayofthedo@gmail.com>
2021-02-01 03:31:11 +0800

    Ver:1.0.1.2102173501

---
+++ craw/main_craw.py
@@ -0,0 +1,24 @@
+now_loading={
+    {
+        'name':'请问您今天要点兔子吗？',
+        'url':'http://m.manhuaju.com/zhiyu/qingwenninjintianyaolaidiantuzima/',
+        'type':'cut_and_decode',
+        'img_src':'http://imgcache.baidu-com-img.com/statics/pic/?p=https%3A%2F%2Fres.gufengmh8.com%2Fimages%2Fcomic%2F42%2F83442%2F148544091636Kw9zqulXhxqox2.jpg&wapif=1&picid=2701&pinyin=qingwenninjintianyaolaidiantuzima&m_httpurl='
+        },
+    {
+        'name':'我们无法一起学习',
+        'url':'http://m.manhuaju.com/lanmu/womenwufayiqixuexi/',
+        'type':'cut_and_decode',
+        'img_src':'http://imgcache.baidu-com-img.com/statics/pic/?p=https%3A%2F%2Fres.xiaoqinre.com%2Fimages%2Fcomic%2F710%2F1419929%2F1603680803oQDAe6nGxMtX-yWX.jpg&wapif=1&picid=12452&pinyin=womenwufayiqixuexi&m_httpurl='
+        },
+    {
+        'name':'NEWGAME',
+        'url':'http://m.manhuaju.com/shaonv/newgame/',
+        'type':'cut_and_decode',
+        'img_src':'http://imgcache.baidu-com-img.com/statics/pic/?p=https%3A%2F%2Fres.xiaoqinre.com%2Fimages%2Fcomic%2F725%2F1449524%2F1608023323kyFrOE6NdUY2LAhz.jpg&wapif=1&picid=2656&pinyin=newgame&m_httpurl='
+        }
+    }
+    
+    
+    
+    
